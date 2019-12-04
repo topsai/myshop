433757702f620b1c97e6abb3e8233bb11f66a08e
hurte <hurte@foxmail.com>
2019-12-05 00:37:21 +0800

    add uwsgi.service

--- myshop/settings.py
+++ myshop/settings.py
@@ -117,10 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+a = os.path.join(BASE_DIR, "static/")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+print("a", a)
+print("BASE_DIR", BASE_DIR)
 print("STATIC_ROOT", STATIC_ROOT)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
